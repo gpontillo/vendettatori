24e1984da37802a8fdca91baa674be1f67c6981c
Narcis Paviliuc <narcispaviliuc@gmail.com>
2023-05-14 15:56:37 +0200

    Correzione nome database e aggiunta campi alla  tabella notizia

--- dbCreation/createTables.sql
+++ dbCreation/createTables.sql
@@ -1,21 +1,15 @@
-IF NOT EXISTS CREATE DATABASE misinformationProject;
-
-IF NOT EXISTS CREATE TABLE utente (
-
-    id INT(10) PRIMARY KEY NOT NULL,
-    nomeUtente CHAR(25) NOT NULL,
-    email CHAR(25) NOT NULL,
-);
+IF NOT EXISTS CREATE DATABASE vendettatori;
 
 IF NOT EXISTS CREATE TABLE notizia (
 
     id INT(10) PRIMARY KEY NOT NULL,
     link CHAR(255) NOT NULL,
-    descrizione CHAR(255) NOT NULL,
-    idUtente INT (10),
-    indiceAttendibilità INT(10),
-    dataPubblicazione DATE,
-    FOREIGN KEY(idUtente) REFERENCES utente(id)
+    descrizione_notizia CHAR(255) NOT NULL,
+    cateogoria_notizia CHAR(255) NOT NULL,
+    indice_attendibilita INT(10) NOT NULL,
+    data_pubblicazione DATE NOT NULL,
+    data_accaduto DATE,
+    coinvolgimento CHAR(255)
 );
 
 
