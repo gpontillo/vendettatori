fa556535e23bf6fc969f9ec40f042f62ca07cf3f
Narcis Paviliuc <narcispaviliuc@gmail.com>
2023-05-14 16:13:25 +0200

    aggiunta nuova tabella

--- dbCreation/createTables.sql
+++ dbCreation/createTables.sql
@@ -5,12 +5,22 @@ IF NOT EXISTS CREATE TABLE notizia (
     id INT(10) PRIMARY KEY NOT NULL,
     link CHAR(255) NOT NULL,
     descrizione_notizia CHAR(255) NOT NULL,
-    cateogoria_notizia CHAR(255) NOT NULL,
+    categoria INT(255) NOT NULL,
     indice_attendibilita INT(10) NOT NULL,
     data_pubblicazione DATE NOT NULL,
     data_accaduto DATE,
-    coinvolgimento CHAR(255)
+    coinvolgimento CHAR(255),
+    FOREIGN KEY(categoria) REFERENCES categoria(categoria)
 );
+
+IF NOT EXISTS CREATE TABLE categoria(
+    id_categoria INT(1O) PRIMARY KEY NOT NULL,
+    descrizione_categoria CHAR(255)
+);
+
+
+
+
 
 
 
