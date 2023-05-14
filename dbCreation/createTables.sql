c6f6837659458f3d5ef490c761cf4b0b4141e36c
Narcis Paviliuc <narcispaviliuc@gmail.com>
2023-05-14 17:54:14 +0200

    adjustament

--- dbCreation/createTables.sql
+++ dbCreation/createTables.sql
@@ -13,12 +13,14 @@ CREATE TABLE IF NOT EXISTS notizia (
     id INT(10) PRIMARY KEY NOT NULL,
     link CHAR(255) NOT NULL,
     descrizione_notizia CHAR(255) NOT NULL,
-    tipo_categoria INT(10) NOT NULL,
+    tipo_categoria INT(10),
     indice_attendibilita INT(10) NOT NULL,
     data_pubblicazione DATE NOT NULL,
     data_accaduto DATE,
     coinvolgimento CHAR(255),
     FOREIGN KEY(tipo_categoria) REFERENCES categoria(id_categoria)
+        ON DELETE SET NULL ON UPDATE CASCADE
+            
 );
 
 
