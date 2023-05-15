7b684cadef9439d5b3d4d05e8ce8fa96ca28a900
Giovanni Pontillo <g.pontillo1@studenti.uniba.it>
2023-05-15 19:06:24 +0200

    Fix grafic for form

--- missinformation-project/views/site/calculate.php
+++ missinformation-project/views/site/calculate.php
@@ -1,35 +1,35 @@
 <?php
+
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
 use yii\model\Categoria;
 use yii\model\Notizia;
 ?>
-<?php $form = ActiveForm::begin(); ?>
-
-    <?= $form->field($model, 'url') ?>
-    
-
-    <div class="form-group">
+<div class="site-contact">
+    <?php $form = ActiveForm::begin(); ?>
+    <div class="mb-3">
+        <?= $form->field($model, 'url') ?>
+    </div>
+    <div class="mb-3">
         <?= Html::submitButton('Submit', ['class' => 'btn btn-primary']) ?>
     </div>
+    <?php ActiveForm::end(); ?>
+    <?php $form = ActiveForm::begin(); ?>
+    <div class="mb-3">
+        <?=
+        $form->field($model, 'category')->dropdownList(
+            [
+                1 => 'Sport',
+                2 => 'Cronaca'
+            ],
+            ['prompt' => 'Select Category']
 
-<?php ActiveForm::end(); ?>
-
-<?php $form = ActiveForm::begin(); ?>
-
-    <?=
-    $form->field($model, 'category')->dropdownList([
-        1 => 'Sport', 
-        2 => 'Cronaca'
-    ],
-    ['prompt'=>'Select Category']
-
-    );?>
-    
-
-    <div class="form-group">
-        <?= Html::submitButton('Submit', ['class' => 'btn btn-primary']) ?>
+        ); ?>
     </div>
-
-<?php ActiveForm::end(); ?>
-
+    <div class="mb-3">
+        <div class="form-group">
+            <?= Html::submitButton('Submit', ['class' => 'btn btn-primary']) ?>
+        </div>
+    </div>
+    <?php ActiveForm::end(); ?>
+</div>
