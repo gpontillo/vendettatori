2eaec62210fab1bd3813553482fce59f1efba680
Narcis Paviliuc <narcispaviliuc@gmail.com>
2023-05-23 09:50:02 +0200

    adding Fonte as new model

---
+++ missinformation-project/models/Fonte.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "fonte".
+ *
+ * @property int $id
+ * @property string $descrizione_fonte
+ */
+class Fonte extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'fonte';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['descrizione_fonte'], 'required'],
+            [['descrizione_fonte'], 'string', 'max' => 500],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'descrizione_fonte' => 'Descrizione Fonte',
+        ];
+    }
+}
