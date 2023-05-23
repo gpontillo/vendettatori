9ca67b9fceeae95557eb472fffa5f9caf4f44a61
Narcis Paviliuc <narcispaviliuc@gmail.com>
2023-05-23 15:06:32 +0200

    addjusting view elements

--- missinformation-project/views/site/calculate-source.php
+++ missinformation-project/views/site/calculate-source.php
@@ -7,12 +7,28 @@ use yii\model\Notizia;
 use yii\model\Fonte;
 ?>
 <div class="site-contact">
-    <?php $form1 = ActiveForm::begin(); ?>
-    <div class="mb-3">
-        <?= $form1->field($model, 'source') ?>
+    <div class="row">
+        <div class="col">
+            <h2>Search sources</h2>
+            <p>
+                With this form, you can search for a source and see if it's affidable or not.
+                Just insert the name the source to search it.
+                If we can't calculate affidability of the source, you will be asked to send us a report of the source and one of our moderator will analise it.
+            </p>
+        </div>
     </div>
-    <div class="mb-3">
-        <?= Html::submitButton('Submit', ['class' => 'btn btn-primary']) ?>
+    <div class="row">
+        <div class="col">
+            <h4>Search by Name</h4>
+            <?php $form = ActiveForm::begin(); ?>
+            <div class="mb-3">
+                <?= $form->field($model, 'source') ?>
+            </div>
+            <div class="mb-3">
+                <?= Html::submitButton('Submit', ['class' => 'btn btn-primary']) ?>
+            </div>
+            <?php ActiveForm::end(); ?>
+        </div>
+        </div>
     </div>
-    <?php ActiveForm::end(); ?>
 </div>
