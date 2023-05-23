44563d1cbb009024e31e978c448cb54d9fb5024b
Narcis Paviliuc <narcispaviliuc@gmail.com>
2023-05-23 17:18:54 +0200

    seeing form

--- missinformation-project/views/site/login.php
+++ missinformation-project/views/site/login.php
@@ -35,15 +35,12 @@ $this->params['breadcrumbs'][] = $this->title;
         ]) ?>
 
         <div class="form-group">
-            <div class="offset-lg-1 col-lg-11">
+            <div>
                 <?= Html::submitButton('Login', ['class' => 'btn btn-primary', 'name' => 'login-button']) ?>
             </div>
         </div>
 
     <?php ActiveForm::end(); ?>
 
-    <div class="offset-lg-1" style="color:#999;">
-        You may login with <strong>admin/admin</strong> or <strong>demo/demo</strong>.<br>
-        To modify the username/password, please check out the code <code>app\models\User::$users</code>.
-    </div>
+    
 </div>
